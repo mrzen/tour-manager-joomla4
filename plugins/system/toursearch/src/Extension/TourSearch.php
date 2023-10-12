f628ba5a7ee79cc35747c8cd19c4745b2c1673d1
Leo Adamek <leo.adamek@mrzen.com>
2023-10-12 14:39:58 +0100

    Working holiday links API

--- plugins/system/toursearch/src/Extension/TourSearch.php
+++ plugins/system/toursearch/src/Extension/TourSearch.php
@@ -1,15 +1,15 @@
 <?php
-namespace RezKit\Tours\Plugins\Search;
+namespace RezKit\Tours\Plugins\Search\Extension;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Router\ApiRouter;
 use Joomla\CMS\Router\SiteRouter;
 use Joomla\Database\DatabaseInterface;
+use Joomla\Database\ParameterType;
 
 class TourSearch extends CMSPlugin
 {
-	public function onAjaxHolidayLinks(): array
+	public function onAjaxHolidaylinks(): array
 	{
 		$input = Factory::getApplication()->input;
 		$ids = explode(',', $input->getString('ids'));
@@ -18,18 +18,21 @@ class TourSearch extends CMSPlugin
 		$db = Factory::getContainer()->get(DatabaseInterface::class);
 		$q = $db->getQuery(true);
 
-		$q->select('id')
+		$q = $q->select('id, rezkitid')
 			->from('#__holidays')
-			->whereIn('rezkitid', $ids);
+			->whereIn('rezkitid', $ids, ParameterType::STRING)
+			->order('id');
 
 		$db->setQuery($q);
-		$records = $db->loadColumn();
+		$records = $db->loadObjectList('rezkitid');
 
 		/** @var SiteRouter $router */
 		$router = Factory::getContainer()->get(SiteRouter::class);
 
-		return array_map(static function (int $id) use ($router) {
-			return $router->build('index.php?option=com_rktours&view=holiday&id=' . $id);
-		}, $records);
+		foreach ( $records as &$record ) {
+			$record->url = $router->build('/?index.php&option=com_rktours&view=holiday&id=' . $record->id)->toString(['path']);
+		}
+
+		return $records;
 	}
 }
