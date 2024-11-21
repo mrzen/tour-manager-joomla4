d66e9a3d1bc834230ffed2ef742b5b62e47d38ac
Leo Adamek <leo.adamek@mrzen.com>
2024-11-21 13:26:44 +0000

    Controller impl

---
+++ components/com_rktours/site/src/Controller/DisplayController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RezKit\Component\Tours\Site\Controller;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Controller\BaseController;
+
+class DisplayController extends BaseController
+{
+	public function display($cachable = false, $urlparams = []): void
+	{
+		$document = Factory::getApplication()->getDocument();
+		$viewName = $this->input->getCmd('view','holidays');
+		$viewFormat = $document->getViewFormat();
+		$view  = $this->getView($viewName, $viewFormat);
+		$view->document = $document;
+
+		$view->display();
+	}
+}
