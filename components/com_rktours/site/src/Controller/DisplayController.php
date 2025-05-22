2fc4142626e0ff546e437f42c6fd0016ed4ae767
Tom Hood <tom.hood@mrzen.com>
2025-05-22 17:20:38 +0100

    Controller test.

--- components/com_rktours/site/src/Controller/DisplayController.php
+++ components/com_rktours/site/src/Controller/DisplayController.php
@@ -10,7 +10,7 @@ class DisplayController extends BaseController
 	public function display($cachable = false, $urlparams = []): void
 	{
 		$document = Factory::getApplication()->getDocument();
-		$type = $this->input->getCmd('type', 'holidays');
+		$type = $this->input->getCmd('type', 'accommodations');
 		$viewName = $this->input->getCmd('view',$type);
 		$viewFormat = $document->getType();
 		$view  = $this->getView($viewName, $viewFormat);
