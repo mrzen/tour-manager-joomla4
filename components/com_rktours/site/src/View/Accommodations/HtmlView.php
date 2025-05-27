9c1687527b72152c0d43accf6468bef7f81ba251
Tom Hood <tom.hood@mrzen.com>
2025-05-27 12:12:39 +0100

    Add additional view.

---
+++ components/com_rktours/site/src/View/Accommodations/HtmlView.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RezKit\Component\RKTours\Site\View\Accommodations;
+defined('_JEXEC') or die;
+
+use \Joomla\CMS\MVC\View\HtmlView as BaseView;
+
+class HtmlView extends BaseView
+{
+	public function display($tpl = null): void
+	{
+		parent::display($tpl);
+	}
+}
