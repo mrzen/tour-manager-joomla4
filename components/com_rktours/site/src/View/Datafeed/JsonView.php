b763b29a8d9fcd8a314247518ac18533822cf96f
Tom Hood <tom.hood@mrzen.com>
2025-09-12 16:00:06 +0100

    HF24-194 Try API key access restrictions.

--- components/com_rktours/site/src/View/Datafeed/JsonView.php
+++ components/com_rktours/site/src/View/Datafeed/JsonView.php
@@ -1,17 +1,38 @@
 <?php
 namespace RezKit\Component\RKTours\Site\View\Datafeed;
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\MVC\View\JsonView as BaseJsonView;
 
 class JsonView extends BaseJsonView
 {
 	public function display($tpl = null): void
 	{
+		$app   = Factory::getApplication();
+		$input = $app->input;
+
+		// Get expected API key from component params
+		$params        = ComponentHelper::getParams('com_rktours');
+		$expectedToken = $params->get('apikey');
+
+		// Get provided token from request
+		$providedToken = $input->getString('token');
+
+		// Validate token
+		if (!$expectedToken || $providedToken !== $expectedToken) {
+			header('HTTP/1.1 403 Forbidden');
+			header('Content-Type: application/json; charset=utf-8');
+			echo json_encode([
+				'status'  => 'error',
+				'message' => 'Invalid or missing token'
+			], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+			$app->close();
+		}
+
 		// Determine the template and query file
-		$app      = Factory::getApplication();
 		$template = $app->getTemplate();
 		$queries  = JPATH_SITE . '/templates/' . $template . '/queries/data_feed_query.php';
 
@@ -34,8 +55,8 @@ class JsonView extends BaseJsonView
 		];
 
 		header('Content-Type: application/json; charset=utf-8');
-		// Encode and output JSON
 		echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
 		exit;
 	}
 }
