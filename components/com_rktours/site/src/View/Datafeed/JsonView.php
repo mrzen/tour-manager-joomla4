0176d3817b79d3a1cd59e53c5f4179c2bff6275c
Tom Hood <tom.hood@mrzen.com>
2025-09-12 15:18:24 +0100

    HF24-194 Restore data query.

--- components/com_rktours/site/src/View/Datafeed/JsonView.php
+++ components/com_rktours/site/src/View/Datafeed/JsonView.php
@@ -3,6 +3,7 @@ namespace RezKit\Component\RKTours\Site\View\Datafeed;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\View\JsonView as BaseJsonView;
 
 class JsonView extends BaseJsonView
@@ -11,18 +12,32 @@ class JsonView extends BaseJsonView
 
 	public function display($tpl = null): void
 	{
+		// Determine the template and query file
+		$app      = Factory::getApplication();
+		$template = $app->getTemplate();
+		$queries  = JPATH_SITE . '/templates/' . $template . '/queries/holidays_query.php';
+
+		// Load holiday data
+		$holidaysData = [];
+		if (file_exists($queries)) {
+			$holidaysData = include $queries;
+		}
+
+		// Ensure it’s an array
+		if (!is_array($holidaysData)) {
+			$holidaysData = [];
+		}
+
+		// Prepare JSON response
+		$response = [
+			'status' => 'ok',
+			'count'  => count($holidaysData),
+			'items'  => $holidaysData
+		];
 
 		header('Content-Type: application/json; charset=utf-8');
-
-		echo json_encode([
-			'status' => 'ok',
-			'count' => 2,
-			'items' => [
-				['id' => 1, 'slug' => 'holiday-a', 'name' => 'Holiday A'],
-				['id' => 2, 'slug' => 'holiday-b', 'name' => 'Holiday B']
-			]
-		], JSON_PRETTY_PRINT);
-
+		// Encode and output JSON
+		echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 		exit;
 	}
 }
